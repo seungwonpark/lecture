ee62b2c4829b25d22d3c30e5e6ae0020d289c1a5
Seungwon Park <psw14041@gmail.com>
2016-08-12 23:08:02 +0900

    #1 해결
    (...)

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
  <!-- /.container -->
 
 <!-- jQuery Version 1.11.1 -->
-<script src="static/js/jquery.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
 <script src="static/js/bootstrap.min.js"></script>
