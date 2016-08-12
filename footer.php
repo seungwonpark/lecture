6eac5b3e121591c949e374bd78f3cc23b88f839d
Seungwon Park <psw14041@gmail.com>
2016-08-12 23:35:42 +0900

    Update asdf

--- footer.php
+++ footer.php
@@ -10,6 +10,7 @@
 	<div class="container">
 		© 2015-<?php echo date('Y'); ?> <a href="https://github.com/hletrd" target="_blank">윤지용</a>,
 		<a href="https://github.com/seungwonpark" target="_blank">박승원</a> / 
-		<a href="https://github.com/seungwonpark/lecture" target="_blank">GitHub에서 코드 보기</a>
+		<a href="https://github.com/seungwonpark/lecture" target="_blank">GitHub에서 코드 보기</a> / 
+		<a href="https://github.com/seungwonpark/lecture/blob/master/README.md#개인정보-사용-현황" target="_blank">개인정보 사용 현황</a>
 	</div>
 </div>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h1> <?php echo date('Y'); ?>.2학기 공통수강학생 확인 </h1>
-				<h4> 상단바를 눌러 이동하세요. </h4>
+				<h4> 상단바를 눌러 이동하세요. (눌리지 않던 버그 수정됨) </h4>
             </div>
         </div>
         <!-- /.row -->
